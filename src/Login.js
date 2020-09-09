ba571bdc7d7bb9722e5568898d045b5e4366bb38
unknown <khushalthepane2000@gmail.com>
2020-09-10 00:50:08 +0530

    Auth start

--- src/Login.js
+++ src/Login.js
@@ -3,26 +3,37 @@ import './Login.css'
 import { Link } from '@material-ui/core'
 
 function Login() {
+
+    const login = e => {
+        e.preventDefault();
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className="login">
-            <Link
+            <Link to="/">
+            <img
             className="login__logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
             alt=""
             />
+            </Link>
             <div className="login__container">
                 <h1>Sign in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="email" />
                     <h5>Password</h5>
                     <input type="password" />
-                    <button>Sign In</button>
+                    <button onClick={login} type="submit" className="signin">Sign In</button>
                 </form>
                 <p>
-                    {/* conditinons */}
+                    {/* conditinons */}By continuing, you agree to Amazon's<a href="#"> Conditions</a> of Use and <a href="#">Privacy Notice</a>.
                 </p>
-                <button>Create your Amazon Account</button>
+                <button onClick={register} className="register">Create your Amazon Account</button>
             </div>
         </div>
 
